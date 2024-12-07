933e13012c49559bac0b5d51b82c484ad88c0568
zak336 <93667785+zak336@users.noreply.github.com>
2024-12-08 03:34:28 +0530

    terms and stuff

--- docs/script.js
+++ docs/script.js
@@ -3,9 +3,15 @@ function toggleTheme() {
     if (body.style.backgroundColor === "white") {
       body.style.backgroundColor = "#1e1e2f";
       body.style.color = "#fff";
+
     } else {
       body.style.backgroundColor = "white";
       body.style.color = "#000";
     }
   }
-  
+
+  function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
